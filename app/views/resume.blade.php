7b6a94a656d5b37978aa436c46715c581281488b
Adam Vega <adam.j.vega@hotmail.com>
2014-10-06 12:50:02 -0500

    initial commit

---
+++ app/views/resume.blade.php
@@ -0,0 +1,15 @@
+
+@extends('layouts.master')
+
+@section('title')
+my awesome resume
+@stop
+
+@section('content')
+
+@stop
+
+@yield('link-to-portfolio')
+	action('PortfolioController@showPortfolio')
+@stop
+
