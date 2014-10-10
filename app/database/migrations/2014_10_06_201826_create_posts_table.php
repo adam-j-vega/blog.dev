8d842b212bb3a76751ce7d830c211bb9bc0459e5
Adam Vega <adam.j.vega@hotmail.com>
2014-10-10 12:13:52 -0500

    commit with corrections

--- app/database/migrations/2014_10_06_201826_create_posts_table.php
+++ app/database/migrations/2014_10_06_201826_create_posts_table.php
@@ -12,7 +12,7 @@ class CreatePostsTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('post', function(Blueprint $table)
+		Schema::create('posts', function(Blueprint $table)
 		{
 			$table->increments('id');
 			$table->string('title', 255);
@@ -28,7 +28,8 @@ class CreatePostsTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('post');
+		Schema::drop('posts');
 	}
 
 }
+?>
--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -12,8 +12,7 @@ class DatabaseSeeder extends Seeder {
 		Eloquent::unguard();
 
 		$this->call('UserTableSeeder');
-		$this->call('TitleContentsSeeder');
-		$this->call('ContentsTitleSeeder');
+		$this->call('PostsTableSeeder');
 	}
 
 }
